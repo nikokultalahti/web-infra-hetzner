b241bc72f9ceb774740c7661d82f931108304596
Niko Kultalahti <hello@nikokultalahti.com>
2025-02-23 13:14:47 +0000

    Removed variables files and updated the specs

--- hcloud.tf
+++ hcloud.tf
@@ -1,23 +1,13 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+}
+
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token   = var.hcloud_token
-}
-
-# Create a server
-resource "hcloud_server" "server" {
-  name        = "server"
-  server_type = "cx22"
-  image       = var.os_type
-  location    = var.location
-  ssh_keys    = [hcloud_ssh_key.default.id]
-  user_data   = "${file("user_data.yaml")}"
-}
-
-# Create a volume
-resource "hcloud_volume" "volume" {
-  name       = "storage"
-  size       = 100
-  server_id  = "${hcloud_server.server.id}"
-  automount  = true
-  format     = "ext4"
+  token   = "SO5jSgXfuJcZjDAypftNtpJ1JSCFshiI23aGAqAcraxV6A2oZ1AbRwVCks2UWhmQ"
 }
