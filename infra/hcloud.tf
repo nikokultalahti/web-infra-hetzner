48db38a5778276861a12695864a5466849d1d952
Niko Kultalahti <hello@nikokultalahti.com>
2025-02-23 13:18:55 +0000

    Restructured the files

---
+++ infra/hcloud.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source = "hetznercloud/hcloud"
+      version = "~> 1.45"
+    }
+  }
+}
+
+# Configure the Hetzner Cloud Provider
+provider "hcloud" {
+  token   = ""
+}
