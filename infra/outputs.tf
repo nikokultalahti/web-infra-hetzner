c48135a10fbc2d1c801446f2af8aa45544244c27
Niko Kultalahti <hello@nikokultalahti.com>
2025-04-23 21:44:57 +0000

    Updated the references to new name

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,13 @@
 output "ipv4_address" {
   description = "The IPv4 address of the server"
-  value       = hcloud_server.server.ipv4_address
-  depends_on  = [hcloud_server.server]
+  value       = hcloud_server.coolify.ipv4_address
+  depends_on  = [hcloud_server.coolify]
 }
 
 output "ipv6_address" {
   description = "The IPv6 address of the server"
-  value       = hcloud_server.server.ipv6_address
-  depends_on  = [hcloud_server.server]
+  value       = hcloud_server.coolify.ipv6_address
+  depends_on  = [hcloud_server.coolify]
 }
 
 output userdata {
