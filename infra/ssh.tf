1bc814289cbedcc25e81efa4514694fa47c238c3
Niko Kultalahti <hello@nikokultalahti.com>
2025-04-23 21:44:23 +0000

    More hardcoded properties to variables

--- infra/ssh.tf
+++ infra/ssh.tf
@@ -1,4 +1,4 @@
 resource "hcloud_ssh_key" "default" {
   name       = "hetzner_key"
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILeSZOsAbw1s34xZHMb4YXW/hpFcCdf1sGw8JKmV77sT"
+  public_key = "${var.ssh_public_key}"
 }
