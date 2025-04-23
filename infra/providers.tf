4baf08b6e7a75a9d55bb07d49db5e44cd84d78cd
Niko Kultalahti <hello@nikokultalahti.com>
2025-04-23 21:43:59 +0000

    Renaming files

--- infra/providers.tf
+++ infra/providers.tf
@@ -9,5 +9,5 @@ terraform {
 
 # Configure the Hetzner Cloud Provider
 provider "hcloud" {
-  token   = "SO5jSgXfuJcZjDAypftNtpJ1JSCFshiI23aGAqAcraxV6A2oZ1AbRwVCks2UWhmQ"
+  token   = "${var.hcloud_token}"
 }
